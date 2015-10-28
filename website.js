5412e365962a6565e769e4d8c4d7dad4b770fb4a
Jeremy Tang <jtang56@hotmail.com>
2015-10-28 15:24:23 -0500

    more

--- website.js
+++ website.js
@@ -1,17 +1,11 @@
-function writeToFile(ip){
+/*function writeToFile(ip){
     var txtFile = "data.txt";
     var file = new File(txtFile);
     
     file.open("w"); // open file with write access
     file.writeln("IP: ", ip);
     file.close();
-}
-var submit = document.getElementById("submit");
-submit.onclick = function () {
-    var id      = document.getElementById("id").value;
-    var content = document.getElementById("content").value;
-    writeToFile(id, content);
-}
+}*/
 
 function square_bigImg(x) {
     x.style.height = "350px";
